3a75b552cfb21c95c7b6b7e56b3183b8a11194f4
Preston McNair <pmcnair@reconwireless.com>
2012-11-04 21:07:11 -0500

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,13 +1,2 @@
-DROP TABLE IF EXISTS `uribookoptions`;
-
-CREATE TABLE `uribookoptions` (
-  `id` varchar(40),
-  `extension` varchar(20),
-  `uridisplayname` varchar(40),
-  `uribookdial' varchar(40),
-	PRIMARY KEY  (`id`)  
-);
-
-INSERT INTO 'uribookoptions' ('id', 'extension' ,'uridisplayname', 'uribookuri') VALUES
-(1, '1200', 'VUC', 'sip/200901@login.zipdx.com')
+;placeholder for future development
 
